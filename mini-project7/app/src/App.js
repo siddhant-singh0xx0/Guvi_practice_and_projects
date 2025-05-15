d830e9db6dd443168bdc66b041e494c3364dc2d3
Siddhant Singh <E21CSEU0614@bennett.edu.in>
2025-05-15 16:35:49 +0530

    commments

--- mini-project7/app/src/App.js
+++ mini-project7/app/src/App.js
@@ -5,39 +5,39 @@ import Navbar from "./components/Navbar";
 import Counter from "./components/Counter";
 import UpdateCounterSeperate from "./components/UpdateCounterSeperate";
 function App() {
-  const courses = [
-    {
-      id: 1,
-      title: "AI",
-      price: 50,
-    },
-    {
-      id: 2,
-      title: "HTML",
-      price: 205,
-    },
-    {
-      id: 3,
-      title: "CSS",
-      price: 500,
-    },
-    {
-      id: 4,
-      title: "Javascript",
-      price: 700,
-    },
-  ];
+  // const courses = [
+  //   {
+  //     id: 1,
+  //     title: "AI",
+  //     price: 50,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "HTML",
+  //     price: 205,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "CSS",
+  //     price: 500,
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Javascript",
+  //     price: 700,
+  //   },
+  // ];
 
-  const getListItem = courses.map((courses) => (
-    <li key={courses.id}>
-      {courses.title} | {courses.price}
-    </li>
-  ));
-  const purchase = () => console.log("Course is purchased");
+  // const getListItem = courses.map((courses) => (
+  //   <li key={courses.id}>
+  //     {courses.title} | {courses.price}
+  //   </li>
+  // ));
+  // const purchase = () => console.log("Course is purchased");
   return (
     <>
       <Navbar />
-      <div className="container">
+      {/* <div className="container">
         <h2>Courses (Mapping)</h2>
         <ul>{getListItem}</ul>
         <button onClick={purchase}>Access Now</button>
@@ -54,7 +54,7 @@ function App() {
         <Counter />
         <hr />
         <UpdateCounterSeperate />
-      </div>
+      </div> */}
     </>
   );
 }
--- mini-project7/app/src/components/Navbar.jsx
+++ mini-project7/app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar Siddhant</div>
+    <div><h1>Siddhant Singh</h1></div>
   )
 }
 
