bd071961775a22343ad50293f40bde6b467b46b4
siddhant-singh0xx0 <e21cseu0614@bennett.edu.in>
2025-06-01 14:42:31 +0530

    minor changes

--- mini-project7/app/src/components/Navbar.jsx
+++ mini-project7/app/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">GUVI</Link>
+        <Link className="navbar-brand" to="/">Live News</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
